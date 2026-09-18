7c272a5d2097fb72a75b9ff2b614ab51370136a8
abdallahatef1 <Abdallahatef09a@gmail.com>
2026-09-19 01:29:01 +0300

    Q3-6

--- Assignmet 2/Program.cs
+++ Assignmet 2/Program.cs
@@ -29,8 +29,79 @@
 
             #endregion
 
+            #region 3 - 6. Create one shipment of each type + read its data
+            Console.WriteLine("--- Standard Shipment ---");
+            string code1 = ReadString("Tracking Code: ");
+            string desc1 = ReadString("Description: ");
+            decimal weight1 = ReadDecimal("Weight (KG): ");
+            decimal fee1 = ReadDecimal("Delivery Fee (EGP): ");
+            DeliveryAddress address1 = ReadAddress();
+            StandardShipment standard = new StandardShipment(code1, desc1, weight1, fee1, address1);
+
+            Console.WriteLine();
+            Console.WriteLine("--- Express Shipment ---");
+            string code2 = ReadString("Tracking Code: ");
+            string desc2 = ReadString("Description: ");
+            decimal weight2 = ReadDecimal("Weight (KG): ");
+            decimal fee2 = ReadDecimal("Delivery Fee (EGP): ");
+            DeliveryAddress address2 = ReadAddress();
+            decimal extraFee = ReadDecimal("Extra Fee (EGP): ");
+            ExpressShipment express = new ExpressShipment(code2, desc2, weight2, fee2, address2, extraFee);
+
+            Console.WriteLine();
+            Console.WriteLine("--- International Shipment ---");
+            string code3 = ReadString("Tracking Code: ");
+            string desc3 = ReadString("Description: ");
+            decimal weight3 = ReadDecimal("Weight (KG): ");
+            decimal fee3 = ReadDecimal("Delivery Fee (EGP): ");
+            DeliveryAddress address3 = ReadAddress();
+            string country = ReadString("Destination Country: ");
+            decimal customsFee = ReadDecimal("Customs Fee (EGP): ");
+            InternationalShipment international = new InternationalShipment(code3, desc3, weight3, fee3, address3, country, customsFee);
+
+
+            #endregion
 
         }
+        static string ReadString(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+
+        static decimal ReadDecimal(string prompt)
+        {
+            decimal value;
+            Console.Write(prompt);
+            while (!decimal.TryParse(Console.ReadLine(), out value))
+            {
+                Console.Write("Invalid number, try again: ");
+            }
+            return value;
+        }
+
+        static int ReadInt(string prompt)
+        {
+            int value;
+            Console.Write(prompt);
+            while (!int.TryParse(Console.ReadLine(), out value))
+            {
+                Console.Write("Invalid number, try again: ");
+            }
+            return value;
+        }
+
+        static DeliveryAddress ReadAddress()
+        {
+            string street = ReadString("Destination Street: ");
+            string city = ReadString("Destination City: ");
+            int building = ReadInt("Building Number: ");
+            return new DeliveryAddress(city, street, building);
+        }
+
+
     }
+
 }
+
 
