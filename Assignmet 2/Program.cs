eb2a26bb07512777784a4ecd548bc0764f912e5a
abdallahatef1 <Abdallahatef09a@gmail.com>
2026-09-19 01:02:48 +0300

    q1qnd2

--- Assignmet 2/Program.cs
+++ Assignmet 2/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            #region             // 1 & 2. Create the DeliveryCenter (name read from the user)
+            Console.Write("Enter Delivery Center Name: ");
+            string centerName = Console.ReadLine();
+            DeliveryCenter center = new DeliveryCenter(centerName);
+            Console.WriteLine();
+
+
+            #endregion
+
         }
     }
 }
