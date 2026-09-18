be3ae5c3728ac4198025ff8db348c20f8cb426e1
abdallahatef1 <Abdallahatef09a@gmail.com>
2026-09-19 01:26:49 +0300

    Part 01 : Theoretical Questions

--- Assignmet 2/Program.cs
+++ Assignmet 2/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            #region             // 1 & 2. Create the DeliveryCenter (name read from the user)
+            #region  Part 01 : Theoretical Questions
+            //1. class is a reference type, while struct is a value type. Classes support inheritance and polymorphism, while structs do not. Classes can have destructors, while structs cannot. Classes are allocated on the heap, while structs are allocated on the stack.
+
+            //2. Inheritance and polymorphism. Large applications reuse code through class hierarchies, and structs can't inherit.
+            //q2
+            //a. shipment
+            //b. ExpressShipment
+            //C STRING TRACKING CODE
+            // D HE MAKE CODE EASY TO WRITE AND READ 
+
+
+
+            #endregion
+
+
+
+            #region  1 & 2. Create the DeliveryCenter (name read from the user)
             Console.Write("Enter Delivery Center Name: ");
             string centerName = Console.ReadLine();
             DeliveryCenter center = new DeliveryCenter(centerName);
@@ -13,6 +29,8 @@
 
             #endregion
 
+
         }
     }
 }
+
